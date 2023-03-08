4a36833b2c63ec337d75dc87ccc8876572597c06
thatonerookiedev <110988678+Thatonerookiedev@users.noreply.github.com>
2023-03-08 13:22:32 -0600

    project setup

---
+++ allishop/src/mainApp/App.tsx
@@ -0,0 +1,13 @@
+import '../css (temporary)/App.css'
+import '../css (temporary)/test.css'
+
+function App() {
+
+  return (
+   <>
+   <div className="one"></div>
+   </>
+  )
+}
+
+export default App
